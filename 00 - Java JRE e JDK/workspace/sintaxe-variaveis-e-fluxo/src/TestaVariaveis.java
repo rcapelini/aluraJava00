d1e78d9c8a28eae6bc97c3dca2da6f70adcc274d
Rodrigo Capelini <rodrigo.capelini@gmail.com>
2021-01-08 13:29:23 -0300

    Primeiro Commit

---
+++ 00 - Java JRE e JDK/workspace/sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,11 @@
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá novo teste");
+		int idade;
+		idade = 37;
+		System.out.println(idade);
+	}
+
+}

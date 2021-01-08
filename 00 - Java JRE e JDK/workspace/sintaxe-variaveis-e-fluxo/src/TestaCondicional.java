b9e0d19f0e66351510394ac9c08b9b33e498bc9c
Rodrigo Capelini <rodrigo.capelini@gmail.com>
2021-01-08 14:39:00 -0300

    Aula 6 conteúdo 7

---
+++ 00 - Java JRE e JDK/workspace/sintaxe-variaveis-e-fluxo/src/TestaCondicional.java
@@ -0,0 +1,16 @@
+
+public class TestaCondicional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testando Condicionais");
+		int idade = 20;
+		if (idade >= 18) {
+			System.out.println("Você tem mais de 18 anos");
+		}
+		else {
+			System.out.println("Você tem menos de 18 anos");
+		}
+	}
+
+}
